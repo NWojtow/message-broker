a33e017fd91b1ecdf77456a477974939ac69cb7b
NWojt <n.wojtowicz098@gmail.com>
2021-03-24 23:24:58 +0100

    ER socket on startup # Added server socket on startup

--- message-broker-core/src/main/java/broker/application/service/Application.java
+++ message-broker-core/src/main/java/broker/application/service/Application.java
@@ -3,10 +3,11 @@ package broker.application.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(scanBasePackages={"broker.configuration.jwt", "broker.configuration.websecurity",
         "broker.application", "broker.datasource", "broker.messages", "broker.subjects", "broker.users"}   )
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
--- message-broker-core/src/main/java/broker/users/control/UserSocketEndpoint.java
+++ message-broker-core/src/main/java/broker/users/control/UserSocketEndpoint.java
@@ -1,11 +1,16 @@
 package broker.users.control;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+@Component
 public class UserSocketEndpoint {
     private ServerSocket serverSocket;
 
+    @PostConstruct
     public void start (int port) throws IOException {
         serverSocket = new ServerSocket(port);
         while(true) {
