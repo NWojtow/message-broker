3f3a312d951541f0d75656f2c3dcd325daddd042
NWojt <n.wojtowicz098@gmail.com>
2021-03-26 00:11:32 +0100

    ER Broker Client # Added tests

--- message-broker-core/src/test/java/broker/users/control/UserSocketEndpoint.java
+++ message-broker-core/src/test/java/broker/users/control/UserSocketEndpoint.java
@@ -3,11 +3,18 @@ package broker.users.control;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 @RunWith(MockitoJUnitRunner.class)
-public class UserNewMessageSocketEndpointTest {
+public class UserSocketEndpoint {
+
+    @Mock
+    ServerSocket serverSocket;
 
     @Before
     public void init() {
@@ -17,5 +24,10 @@ public class UserNewMessageSocketEndpointTest {
     @Test
     public void shouldSendNewMessage() {
         new UserNewMessageSocketEndpoint("message", "127.0.0.1");
+    }
+
+    @Test
+    public void shouldRecieveNewMessage() throws IOException {
+        new ClientHandler(serverSocket.accept());
     }
 }
